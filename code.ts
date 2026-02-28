b9bbb651b8e0d0027ecfc4fd25eda0247a815484
Pedro Palmier <58957382+pedropalmier@users.noreply.github.com>
2026-02-28 12:05:36 -0300

    feat: add formatting toggle and copy button

--- code.ts
+++ code.ts
@@ -1,10 +1,8 @@
 figma.showUI(__html__);
-figma.ui.resize(188, 64);
+figma.ui.resize(369, 64);
 
 figma.ui.onmessage = msg => {
   if (msg.type === 'copy') {
-    figma.notify('CPF copied!');
-
     figma.ui.postMessage({ type: 'copy-to-clipboard', text: msg.text });
   }
 };
